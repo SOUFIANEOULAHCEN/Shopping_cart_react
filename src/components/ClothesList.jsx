727866a74e6e42cf8e223ec4c57e43ce5a76ce15
SOUFIANEOULAHCEN <soufiane.oulahcen@gmail.com>
2024-12-15 02:41:01 +0100

    petiti modification sur ClothesList.jsx

--- src/components/ClothesList.jsx
+++ src/components/ClothesList.jsx
@@ -5,7 +5,7 @@ import { FaAngleLeft } from "react-icons/fa";
 import { useState } from "react";
 export default function ClothesList() {
   const [CurrentPage, SetCurrentPAge] = useState(1); // Page courante
-  const NmbreItems = 6; // Nombre de produits par page
+  const NmbreItems = 8; // Nombre de produits par page
   const StartIndex = (CurrentPage - 1) * NmbreItems;
   const CurrentProducts = ProductData.slice(
     StartIndex,
@@ -13,41 +13,45 @@ export default function ClothesList() {
   );
   return (
     <div className="px-[10%] py-4">
-      <div className="grid grid-cols-3  gap-4">
+      <div className="grid grid-cols-4  gap-3">
         {CurrentProducts.map((pro, i) => (
           <Card key={i} pro={pro} />
         ))}
       </div>
 
-      <div className="w-full px-10 py-2 flex items-center justify-center pt-6">
-        <div className="flex gap-4">
-          <button
-            onClick={() => {
-              SetCurrentPAge(CurrentPage - 1);
-            }}
-            disabled={CurrentPage === 1}
-            className="p-2 rounded-full  hover:bg-gray-300 transition duration-200 shadow-md hover:shadow-lg"
-          >
-            <FaAngleLeft
-              className="text-gray-600 hover:text-gray-800"
-              size={25}
-            />
-          </button>
-          <button
-            onClick={() => {
-              SetCurrentPAge(CurrentPage + 1);
-            }}
-            disabled={
-              CurrentPage === Math.ceil(ProductData.length / NmbreItems)
-            }
-            className="p-2 rounded-full hover:bg-gray-300 transition duration-200 shadow-md hover:shadow-lg"
-          >
-            <FaAngleRight
-              className="text-gray-600 hover:text-gray-800"
-              size={25}
-            />
-          </button>
-        </div>
+      <div className="flex items-center justify-center pt-3 gap-4">
+        <button
+          onClick={() =>
+            SetCurrentPAge((prevPage) => Math.max(prevPage - 1, 1))
+          }
+          disabled={CurrentPage === 1}
+          className={`p-2 rounded-full hover:bg-gray-300 transition duration-200 shadow-md hover:shadow-lg ${
+            CurrentPage === 1 ? "opacity-50 cursor-not-allowed" : ""
+          }`}
+        >
+          <FaAngleLeft
+            className="text-gray-600 hover:text-gray-800"
+            size={25}
+          />
+        </button>
+        <button
+          onClick={() =>
+            SetCurrentPAge((prevPage) =>
+              Math.min(prevPage + 1, Math.ceil(ProductData.length / NmbreItems))
+            )
+          }
+          disabled={CurrentPage === Math.ceil(ProductData.length / NmbreItems)}
+          className={`p-2 rounded-full hover:bg-gray-300 transition duration-200 shadow-md hover:shadow-lg ${
+            CurrentPage === Math.ceil(ProductData.length / NmbreItems)
+              ? "opacity-50 cursor-not-allowed"
+              : ""
+          }`}
+        >
+          <FaAngleRight
+            className="text-gray-600 hover:text-gray-800"
+            size={25}
+          />
+        </button>
       </div>
     </div>
   );
